5754f3acf6101f28f25a3e89dcd08d3f77914791
Jonathan Enrique <jonathan.ariel.enrique@gmail.com>
2023-06-08 13:25:24 -0300

    Agregando ejercicios

--- Unidad3_PasajeDePamametros/main.c
+++ Unidad3_PasajeDePamametros/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define TAM_VEC 10
+#define TAM_VEC_PERSONA 15
+
 
 typedef struct
 {
@@ -11,17 +14,42 @@ typedef struct
     float peso;
 }Persona;
 
-int compararEntero(const void* arg1, const void* arg2);
-int compararPersona(const void* arg1, const void* arg2);
+int compararEntero(const void* arg1, const void* arg2); // Ejercicio 3.9
+int compararPersonaDNI(const void* arg1, const void* arg2); // Ejercicio 3.10
+int compararPersonaNombreYApellido(const void* arg1, const void* arg2); // Ejercicio 3.11
+int buscarXdni(const Persona *p, Persona *d); // No le hace falta la cantidad de elementos?? ->
+
 void mostrarVecEntero(int* vec, int ce);
+void mostrarVecPer(Persona* vecPer, int ce);
 
 int main()
 {
     int vec[TAM_VEC] = {2, 1, 3, 5, 6, 8, 9, 10, 7, 4};
+    Persona vecPer[TAM_VEC_PERSONA] = { {12345678, "Lopez", "Juan", 70.5},
+        {98765432, "Garcia", "Maria", 62.3},
+        {56789012, "Rodriguez", "Carlos", 80.0},
+        {34567890, "Fernandez", "Luisa", 55.8},
+        {78901234, "Gonzalez", "Pedro", 68.2},
+        {55555555, "Torres", "Ana", 58.6},
+        {22222222, "Martinez", "Pablo", 75.0},
+        {99999999, "Sanchez", "Laura", 61.4},
+        {11111111, "Perez", "Lucia", 63.7},
+        {22223333, "Romero", "Gabriel", 67.9},
+        {44445555, "Luna", "Valeria", 55.2},
+        {77778888, "Silva", "Diego", 72.6},
+        {99999988, "Mendoza", "Sofia", 59.8},
+        {11110000, "Cruz", "Matias", 64.1},
+        {34543678, "Lopez", "Alberto", 70.5} };
 
     qsort(vec, TAM_VEC, sizeof(vec[0]), compararEntero);
+    qsort(vecPer, TAM_VEC_PERSONA, sizeof(Persona), compararPersonaNombreYApellido);
+    Persona buscado = {12345678, "LopezModificado", "JuanModificado", 87.03};
+    buscarXdni(vecPer, &buscado);
 
+    printf("Ordenamiento de enteros:\n");
     mostrarVecEntero(vec, TAM_VEC);
+    printf("Ordenamiento de personas:\n");
+    mostrarVecPer(vecPer, TAM_VEC_PERSONA);
 
     return 0;
 }
@@ -42,9 +70,44 @@ void mostrarVecEntero(int* vec, int ce)
     printf("\n");
 }
 
-int compararPersona(const void* arg1, const void* arg2)
+int compararPersonaDNI(const void* arg1, const void* arg2)
 {
     return ((Persona*)arg1)->dni - ((Persona*)arg2)->dni;
 }
 
-// Prueba
+void mostrarVecPer(Persona* vecPer, int ce)
+{
+    Persona* finVec = vecPer + ce;
+
+    for(Persona* i = vecPer; i < finVec; i++){
+        printf("%d %s %s %f", i->dni, i->apellido, i->nombres, i->peso);
+        printf("\n");
+    }
+}
+
+int compararPersonaNombreYApellido(const void* arg1, const void* arg2)
+{
+    int cmpNom = strcmp(((Persona*)arg1)->nombres, ((Persona*)arg2)->nombres);
+    int cmpApell = strcmp(((Persona*)arg1)->apellido, ((Persona*)arg2)->apellido);
+
+    //return cmpNom != 0 ? cmpNom : cmpApell;
+    return cmpApell != 0 ? cmpApell : cmpNom;
+}
+
+int buscarXdni(const Persona *p, Persona *d)
+{
+    int encontrado = 0;
+    Persona* finVecPer = (Persona*)p + TAM_VEC_PERSONA - 1;
+
+    for(Persona* i = (Persona*)p; i <= finVecPer && !encontrado; i++){
+        if(i->dni == d->dni){
+            strcpy(i->nombres, d->nombres);
+            strcpy(i->apellido, d->apellido);
+            i->peso = d->peso;
+
+            encontrado = 1;
+        }
+    }
+
+    return encontrado;
+}
