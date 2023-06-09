f12db27124b001595749cc8be2c18dc1f43fef7a
NicolasDC0 <thenumber9875@hotmail.com>
2023-06-09 16:01:28 -0300

    Avance de ejercicios

---
+++ Guia_TP/Unidad1/main.c
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+bool eliminarPrimeraAparicionDeVector(const int* vec, int* ce, int tamVec, int elem); // Ejercicio 1.4 Jona
+void eliminarPrimerAparacionElemento (int cadena[], int elemento, int tamCadena); // Ejercicio 1.4 Nico
+void eliminarTodasAparacionesElemento (int cadena[], int elemento, int tamCadena); // Ejercicio 1.5 Nico
+void eliminarTodasAparacionesElementoConPuntero(int* vec, int* ce, int tamVec, int elemento); // Ejercicio 1.5 con punteros Nico
+bool detectarPalindromo(const char* vec, const int* ce); // Ejercicio 1.6 funcion adicional Nico
+/// EJERCICIO 1.7 ES ATOI
+int detectarPalabra(const char* cadena, const char* palabra, int tamPalabra);
+size_t mi_strlen (char* cadena);
+
+int main()
+{
+    int cadenaPrueba[10] = {1, 5, 6, 7, 8, 9, 10, 5, 7, 5};
+    char cadenaPrueba2[] = "A luna dse anula";
+    char cadenaPrueba3[] = "LaaL";
+    char cadenaPrueba4[] = "un millon ciento veinte mil cuatrocientos veinti y dos";
+    char cadenaPrueba5[] = "tres";
+    char cadenaPrueba6[] = "Buscaremos la palabra Lepricon, en respecto a su significado el Lepricon es...";
+    char palabraPrueba6[] = "Lepricon";
+
+    int ce = 10, tamVec = 10, ceCP2 = 16, ceCP3 = 4, resultadoSumaCadenas = 0,testSumaCadenas = 0, tamCadena = 0, tamCadena2 = 0, halladas = 0;
+    bool valor;
+
+    //valor=eliminarPrimeraAparicionDeVector(cadenaPrueba, &ce, tamVec,  5);
+    //eliminarPrimerAparacionElemento(cadenaPrueba, 7, 10);
+    //eliminarTodasAparacionesElemento(cadenaPrueba, 5, 10);
+    eliminarTodasAparacionesElementoConPuntero(cadenaPrueba, &ce, tamVec, 5);
+    valor = detectarPalindromo(cadenaPrueba2, &ceCP2);
+    valor = detectarPalindromo(cadenaPrueba3, &ceCP3);
+    halladas = detectarPalabra(cadenaPrueba6, palabraPrueba6, mi_strlen(palabraPrueba6));
+
+    puts("Fin de Prueba");
+
+    return 0;
+}
+
+bool eliminarPrimeraAparicionDeVector(const int* vec, int* ce, int tamVec, int elem)
+{
+    int* cVec = (int*)vec;
+    int* finElemsVec = (int*)vec + *ce - 1;
+    int encontrado = 0;
+
+    while(cVec <= finElemsVec)
+    {
+        if(encontrado || *cVec == elem)
+        {
+            *cVec = *(cVec + 1);
+            encontrado = 1;
+        }
+        cVec++;
+    }
+
+    return encontrado ? (*ce = (*ce) - 1, true) : false;
+}
+
+void eliminarPrimerAparacionElemento (int cadena[], int elemento, int tamCadena)
+{
+    int i, j;
+
+    for(i = 0; cadena[i] != elemento; i++) // busca el elemento en cadena.
+        ;
+
+    for(j = i; j < (tamCadena-(1)); j++) // elimina elemento desplazando el valor siguiente en cadena a la izquierda.
+    {
+        cadena[j]=cadena[j+1];
+    }
+
+    cadena[j]=0; // se rellena ultima posicion que quedaria con valor basura (REDUNDANTE SI INCORPORAMOS CE para saber valores validos).
+}
+
+void eliminarTodasAparacionesElemento(int cadena[], int elemento, int tamCadena)
+{
+    int i, d, j, cantidadElementos = 0;
+
+    for(i = 0; i < tamCadena; i++)  // buscamos todas las aparaciones del elemento en vector
+    {
+        if(cadena[i] == elemento)
+            cantidadElementos++;
+    }
+
+    while(cantidadElementos != 0) // usamos el contador de elementos hallados para hacer varias pasadas
+    {
+        for(d = 0; cadena[d] != elemento; d++)
+            ;
+        for(j = d; j < tamCadena-1; j++)
+            cadena[j] = cadena[j+1];
+        cantidadElementos--;
+    }
+
+    for(d = 0; d < tamCadena; d++) // se rellena posiciones que quedarian vacias o con valor redundante. (REDUNDANTE SI INCORPORAMOS CE para saber valores validos)
+        cadena[d]=0;
+}
+
+void eliminarTodasAparacionesElementoConPuntero(int* vec, int* ce, int tamVec, int elemento) /// DUDA: tamVec lo usamos para algo?
+{
+    int* posFinalVector = vec + (*ce - 1);
+    int* vecIndice1 = vec, * vecIndice2 = vec;
+    int cantidadElementos = 0, encontrado = 0, d;
+
+    while(vecIndice1 <= posFinalVector)
+    {
+        if(*vecIndice1 == elemento)
+            cantidadElementos++;
+        vecIndice1++;
+    }
+
+    for(d = 0; d != cantidadElementos; d++)
+    {
+        while(vecIndice2 <= posFinalVector)
+        {
+            if(*vecIndice2 == elemento || encontrado == 1)
+            {
+                *vecIndice2 = *(vecIndice2 + 1);
+                encontrado = 1;
+            }
+            vecIndice2++;
+        }
+        encontrado = 0;
+        vecIndice2 = vec;
+    }
+    *ce = (*ce) - cantidadElementos;
+
+    /*
+    for(vecIndice1 = vec; vecIndice1 <= posFinalVector; vecIndice1++)  // buscamos todas las aparaciones del elemento en vector
+    {
+        if(*vecIndice1 == elemento)
+            cantidadElementos++;
+    }
+
+    for(d = 0; d != cantidadElementos; d--)
+    {
+        for(vecIndice2 = vec; vecIndice2 < posFinalVector; vecIndice2++)
+        {
+            if(*vecIndice2 == elemento || encontrado == 1)
+            {
+                *vecIndice2 = *(vecIndice2 + 1);
+                encontrado = 1;
+            }
+        }
+        encontrado = 0;
+    }
+    *ce = (*ce) - cantidadElementos;
+    */ /// REVISAR PORQUE NO ANDA CON FOR (OJO PUDO SER PORQUE HICE MAL CALCULO DE ULTIMA POSICION DE VECTOR)
+}
+
+bool detectarPalindromo(const char* vec, const int* ce)
+{
+    char* vecInicio = (char*)vec;
+    char* vecFinal = (char*)vec + (*ce - 1);
+    int noPalindromo = 0;
+
+    while(vecInicio != vecFinal && noPalindromo != 1 && vecInicio < vecFinal)
+    {
+        if(*vecInicio == ' ')
+        {
+            vecInicio++;
+        }
+        if(*vecFinal == ' ')
+        {
+            vecFinal--;
+        }
+        if(tolower(*vecInicio) != tolower(*vecFinal))
+        {
+            noPalindromo = 1;
+        }
+        vecInicio++;
+        vecFinal--;
+    }
+
+    return noPalindromo ? false : true;
+}
+
+int detectarPalabra(const char* cadena, const char* palabra, int tamPalabra)
+{
+    int halladas = 0, coincidencias = 0;
+    char* cursorCad;
+    char* cursorPal = (char*) palabra;
+
+    for(cursorCad = (char*) cadena; *cursorCad != '\0'; cursorCad++)
+    {
+
+        if(*cursorCad == *cursorPal)
+        {
+            coincidencias++;
+            cursorPal++;
+        }
+
+        if(coincidencias == tamPalabra) // se podria tomar *cursorPal == '\0' para tomar como hallado?
+        {
+            halladas++;
+            coincidencias = 0;
+            cursorPal = (char*) palabra;
+        }
+    }
+
+    return halladas;
+}
+
+size_t mi_strlen (char* cadena)
+{
+    char* cursorCad = cadena;
+    while (*cursorCad != '\0')
+        cursorCad++;
+    return cursorCad - cadena; // para referencia ver 5.4 de Kernighan
+}
